897ff55f5c50fbd578e56b67c0f0af2456601952
CalCod3 <calnjoroge@gmail.com>
2022-02-04 12:32:00 +0300

    Updates to the template(fonts) and handling of forms

--- schoolanding/models.py
+++ schoolanding/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Contact(models.Model):
     first_name = models.CharField(max_length=20, null=True)
     last_name = models.CharField(max_length=20, null=True)
-    email = models.EmailField
-    message = models.CharField(max_length=300)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    message = models.TextField(max_length=300,blank=True, null=True)
 
     def __str__(self):
         return self.first_name
--- schoolnet/settings.py
+++ schoolnet/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'widget_tweaks',
+    'fontawesome_free',
 ]
 
 MIDDLEWARE = [
